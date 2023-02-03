0e1fb45592d4152a55b74e951c54434e7806aa19
greenpickles03 <71419350+greenpickles03@users.noreply.github.com>
2023-02-03 14:17:06 +0800

    commit 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularBasics';
   message = 'Hello Angular.';
+  fontColor = 'blue';
+  sayHelloId = 1;
+  canClick = false;
+  message2 = 'Hello World';
+  message3 = "I'm ready only";
+  canEdit = false;
+
+  onEditClick(){
+    this.canEdit = !this.canEdit;
+    if(this.canEdit){
+      this.message3 = "You can edit me";
+    }else{
+      this.message3 = "I'm ready only";
+    }
+  }
+
+  sayMessage() {
+    alert(this.message2)
+  }
+
 }
